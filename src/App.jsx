8e90c6fe5cec5aadc5950e49babc1305a96d8975
jxwib <jumanhsami@outlook.com>
2026-02-05 01:45:34 +0300

    clean project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Function Component for App
+//quick test for localhost:3000
+
+function App() {
+  return (
+    <div className="App">
+      
+          <h1>Hello World!</h1>
+
+    </div>
+  );
+}
+
+export default App;
